2bdad3f561869ec5a9005163d204e10029a3bb7f
anggriawanmr <anggriawanmr@gmail.com>
2023-02-28 11:00:29 +0700

    Adding BlockRenderer component :sparkles:

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,30 @@
-export default function Home() {
+import { gql } from '@apollo/client';
+
+import client from '../../client';
+
+export default function Home(props) {
+  console.log('Props: ', props);
   return <div>Next JS &amp; WordPress course.</div>;
 }
+
+export const getStaticProps = async () => {
+  const { data } = await client.query({
+    query: gql`
+      query NewQuery {
+        nodeByUri(uri: "/") {
+          ... on Page {
+            id
+            title
+            blocksJSON
+          }
+        }
+      }
+    `,
+  });
+
+  return {
+    props: {
+      blocks: JSON.parse(data.nodeByUri.blocksJSON),
+    },
+  };
+};
