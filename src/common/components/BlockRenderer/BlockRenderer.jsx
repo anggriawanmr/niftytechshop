8ccb93a9b85ecef0e95d710870f79be0c9efa58c
anggriawanmr <anggriawanmr@gmail.com>
2023-02-28 17:15:59 +0700

    Adding Heading component :sparkles:

--- src/common/components/BlockRenderer/BlockRenderer.jsx
+++ src/common/components/BlockRenderer/BlockRenderer.jsx
@@ -1,9 +1,14 @@
 import { Cover } from '../Cover';
+import { Heading } from '../Heading';
 
 export const BlockRenderer = ({ blocks }) => {
   return blocks.map((block) => {
     switch (block.name) {
+      case 'core/heading': {
+        return <Heading key={block.id}></Heading>;
+      }
       case 'core/cover': {
+        console.log('Block: ', block);
         return (
           <Cover key={block.id} background={block.attributes.url}>
             core cover
