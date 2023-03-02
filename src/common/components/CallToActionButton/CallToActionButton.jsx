d8026967390d220c439d91bc1b1f57ea335cf112
anggriawanmr <anggriawanmr@gmail.com>
2023-03-02 14:04:28 +0700

    Editing CallToActionButton component :hammer:

--- src/common/components/CallToActionButton/CallToActionButton.jsx
+++ src/common/components/CallToActionButton/CallToActionButton.jsx
@@ -1,3 +1,18 @@
-export const CallToActionButton = () => {
-  return <div>CTA Button</div>;
+import { ButtonLink } from '../ButtonLink';
+
+export const CallToActionButton = ({
+  align = 'left',
+  buttonLabel,
+  destination,
+}) => {
+  const alignMap = {
+    left: 'text-left',
+    center: 'text-center',
+    right: 'text-right',
+  };
+  return (
+    <div className={alignMap[align]}>
+      <ButtonLink destination={destination} label={buttonLabel} />
+    </div>
+  );
 };
