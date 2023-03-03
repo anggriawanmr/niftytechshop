5a71285d5004ed8dfd32e45192ea360aea70a095
anggriawanmr <anggriawanmr@gmail.com>
2023-03-03 14:56:53 +0700

    Adding search api :sparkles:

--- src/common/components/ProductSearch/ProductSearch.jsx
+++ src/common/components/ProductSearch/ProductSearch.jsx
@@ -1,3 +1,14 @@
+import { useEffect } from 'react';
+
 export const ProductSearch = () => {
+  useEffect(() => {
+    const search = async () => {
+      const response = await fetch(`/api/search`);
+      const data = await response.json();
+      console.log('Search data: ', data);
+    };
+    search();
+  }, []);
+
   return <div>Product Search</div>;
 };
---
+++ src/pages/api/search.js
@@ -0,0 +1,38 @@
+import { gql } from '@apollo/client';
+import client from 'client';
+
+const handler = async (req, res) => {
+  try {
+    const { data } = await client.query({
+      query: gql`
+        query MainMenuQuery {
+          products {
+            nodes {
+              databaseId
+              title
+              uri
+              featuredImage {
+                node {
+                  uri
+                }
+              }
+              productFeatures {
+                core
+                memory
+                price
+                ram
+              }
+            }
+          }
+        }
+      `,
+    });
+    return res.status(200).json({
+      products: data.products.nodes,
+    });
+  } catch (error) {
+    console.log('Error: ', error);
+  }
+};
+
+export default handler;
