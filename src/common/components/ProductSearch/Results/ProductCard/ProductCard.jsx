e9f97fd26b376522ad4b655e9976a20ad3b2a5ee
anggriawanmr <anggriawanmr@gmail.com>
2023-03-05 20:28:17 +0700

    Adding more details on ProductCard component :hammer:

--- src/common/components/ProductSearch/Results/ProductCard/ProductCard.jsx
+++ src/common/components/ProductSearch/Results/ProductCard/ProductCard.jsx
@@ -1,5 +1,12 @@
+import {
+  faHardDrive,
+  faMemory,
+  faMicrochip,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Image from 'next/image';
 import Link from 'next/link';
+import numeral from 'numeral';
 
 const ProductCard = ({
   title,
@@ -14,7 +21,27 @@ const ProductCard = ({
     <Link href={destination}>
       <a className="border-2 border-slate-300 p-5 bg-slate-100 hover:bg-slate-200">
         <div className="flex w-full">
-          <Image src={image} height="200px" width="300px" objectFit="cover" />
+          <Image
+            src={image}
+            height="200px"
+            width="300px"
+            objectFit="cover"
+            alt=""
+          />
+        </div>
+        <div className="mt-3 text-lg font-bold">{title}</div>
+        <div className="text-lg">Rp.{numeral(price).format('0,0')}</div>
+        <div className="text-sm mt-3">
+          <FontAwesomeIcon icon={faMicrochip} />
+          <span className="pl-2">{core}</span>
+        </div>
+        <div className="text-sm mt-3">
+          <FontAwesomeIcon icon={faMemory} />
+          <span className="pl-2">{ram}GB</span>
+        </div>
+        <div className="text-sm mt-3">
+          <FontAwesomeIcon icon={faHardDrive} />
+          <span className="pl-2">{memory} GB</span>
         </div>
       </a>
     </Link>
